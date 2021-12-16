ab67d87c65d8f4579a09b3c0c1cc5c3ae733b14c
didikmulyadi <didikmulyadi12@gmail.com>
2021-12-16 16:49:56 +0700

    Remove gosend config

--- src/config/request.ts
+++ src/config/request.ts
@@ -1,17 +1,9 @@
 import axios from 'axios';
-import config from './config';
-
-const { API_BASE_URL, CLIENT_ID, PASS_KEY } = config.gosend;
 
 const request = axios.create({
   timeout: 15000,
-  baseURL: API_BASE_URL,
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-    'Client-ID': CLIENT_ID,
-    'Pass-Key': PASS_KEY,
-  },
+  baseURL: 'https://',
+  headers: {},
 });
 
 export default request;
